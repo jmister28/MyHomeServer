836d90d89e60e0d2d71b5d0e9881d78104608b61
jmister28 <jmister28@users.noreply.github.com>
2015-04-14 10:40:59 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 
 int main()
 {
+  printf("LOADING... \n");
+  //Loading is to test computer speed
+  clrscr();
+  // getch(); function
+  printf("Press Any Key To Continue...");
   
+  // Press any key to continue to clrscr();
+  getch();
+  clrscr();
+  
+  // server ip
+  fscanf(stdin)
+  
+  return 0;
 }
